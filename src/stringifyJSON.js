1cadcb28b4c877af91a61ab3471268ee17f57c16
2000prcs <2000prcs@gmail.com>
2018-03-05 15:31:26 -0800

    Complete stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,50 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  //find data type by using if statement
+  if (typeof obj === 'number' || obj === null || typeof obj === 'boolean') {
+    return '' + obj;
+  } else if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  } else if (Array.isArray(obj)) {
+  //for number, typeof = number 
+  //use string concatanation '' + 9
+  //using Array.isArray to check if it's array 
+    if (obj.length === 0) {
+      return '' + '[]';
+    } else if (obj.length > 0) {
+      var output = [];
+      for (var i = 0; i < obj.length; i++) {
+        output.push(stringifyJSON(obj[i]));
+      }
+      return '[' + output + ']';
+    }
+  } else if (obj instanceof Object) {
+    if (Object.keys(obj).length === 0) {
+      return '' + '{}';
+    } else {
+      var keys = Object.keys(obj);
+      var output = '';
+      // for(var key in obj){
+      for (var i = 0; i < keys.length; i++) {
+        if (obj[keys[i]] instanceof Function || obj[keys[i]] === undefined) {
+          return '' + '{}';
+        }
+        if (keys.length === 1) {
+          output += '"' + keys[i] + '"' + ':' + stringifyJSON(obj[keys[i]]);
+        } else {
+          if (i === keys.length - 1) {
+            output += '"' + keys[i] + '"' + ':' + stringifyJSON(obj[keys[i]]);
+          } else {
+            output += '"' + keys[i] + '"' + ':' + stringifyJSON(obj[keys[i]]) + ',';
+          }
+        }
+        
+      }
+      return '{' + output + '}';
+    }
+  }
+  return obj;
+  //if it's an array return an empty array 
+  //if item is in array, iterate through the array
 };
